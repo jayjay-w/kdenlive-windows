1e0d2a8b5daa5857c9e5046f4a84d6a8a1eab374
Joseph W. Joshua <joejoshw@gmail.com>
2016-06-15 18:04:07 +0200

    Changes to build script to copy mxe files to the mxe folder
    Signed-off-by: Joseph W. Joshua <joejoshw@gmail.com>

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #script to compile kdenlive on windows
 #Step 1: Checkout mxe from github, if not already done
 if [ ! -d "mxe" ]; then
+        echo "Fetching MXE sources"
 	mkdir  ./mxe
 	cd ./mxe
 	git clone https://github.com/mxe/mxe.git
 	cd ..
+else
+    echo "MXE folder already exists. Pulling latest changes."
+    cd ./mxe/mxe
+    git pull
+    cd ../../
 fi
-cd ./mxe
-git remote remove vpinon
-git remote add vpinon git@git.kde.org:scratch/vpinon/kdenlive-windows
-git fetch vpinon
-git merge vpinon/kdenlive
+#Copy out makefiles to mxe
+echo "Copying our makefiles to mxe"
+cp ./*.mk ./mxe/mxe/src -f
+cd ./mxe/mxe
 #Step 2: compile the needed stuff, mlt, frameworks etc
+echo "Compiling MLT and its dependencies"
 make mlt
 #Step 6: clone and compile kdenlive
