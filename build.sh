edaa8fde5959574a00fbad10cd29eeae0d8a6c3c
Joseph W. Joshua <joejoshw@gmail.com>
2016-06-17 23:16:10 +0200

    Added most of the frameworks
    Signed-off-by: Joseph W. Joshua <joejoshw@gmail.com>

--- build.sh
+++ build.sh
@@ -28,71 +28,85 @@ make qt5 nsis mlt
 #Step 6: clone and compile kde frameworks
 
 function buildFramework {
-echo "Compiling KDE frameworks - $1"
+cd $BUILD_DIR
+echo "Compiling KDE framework ($1)"
 #check if folder already exists and pull, if not, clone
-if [ ! -d "$BUILD_DIR/$1" ]; then
+if [ ! -d "$1" ]; then
     echo "Cloning $1"
-    git clone git://anongit.kde.org/$1.git $BUILD_DIR/$1
+    git clone git://anongit.kde.org/$1.git $1
 else
     echo "Pulling $1"
-    cd $BUILD_DIR/$1
+    cd $1
     git pull
 fi
-mkdir -p $BUILD_DIR/$1_win
-cd $BUILD_DIR/$1_win
-cmake ../$1 \
-    -DCMAKE_TOOLCHAIN_FILE=~/src/mxe/usr/i686-w64-mingw32.static/share/cmake/mxe-conf.cmake \
+
+cd $BUILD_DIR
+mkdir -p $1_win
+cd $1_win
+cmake $BUILD_DIR/$1 \
+    -DCMAKE_TOOLCHAIN_FILE=~/src/mxe/usr/i686-w64-mingw32.shared/share/cmake/mxe-conf.cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=../usr \
-    -DLIB_INSTALL_DIR=../lib \
-    -DLIBEXEC_INSTALL_DIR=../lib \
+    -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/usr \
+    -DLIB_INSTALL_DIR=$BUILD_DIR/lib \
+    -DLIBEXEC_INSTALL_DIR=$BUILD_DIR/lib \
     -DKDE_INSTALL_USE_QT_SYS_PATHS=ON \
     -DBUILD_TESTING=OFF
     
-    cmake ../$1 \
-    -DCMAKE_TOOLCHAIN_FILE=~/src/mxe/usr/i686-w64-mingw32.static/share/cmake/mxe-conf.cmake \
+    cmake $BUILD_DIR/$1 \
+    -DCMAKE_TOOLCHAIN_FILE=~/src/mxe/usr/i686-w64-mingw32.shared/share/cmake/mxe-conf.cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=../usr \
-    -DLIB_INSTALL_DIR=../lib \
-    -DLIBEXEC_INSTALL_DIR=../lib \
+    -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/usr \
+    -DLIB_INSTALL_DIR=$BUILD_DIR/lib \
+    -DLIBEXEC_INSTALL_DIR=$BUILD_DIR/lib \
     -DKDE_INSTALL_USE_QT_SYS_PATHS=ON \
     -DBUILD_TESTING=OFF
 
-    cmake ../$1 \
-    -DCMAKE_TOOLCHAIN_FILE=~/src/mxe/usr/i686-w64-mingw32.static/share/cmake/mxe-conf.cmake \
+    cmake $BUILD_DIR/$1 \
+    -DCMAKE_TOOLCHAIN_FILE=~/src/mxe/usr/i686-w64-mingw32.shared/share/cmake/mxe-conf.cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=../usr \
-    -DLIB_INSTALL_DIR=../lib \
-    -DLIBEXEC_INSTALL_DIR=../lib \
+    -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/usr \
+    -DLIB_INSTALL_DIR=$BUILD_DIR/lib \
+    -DLIBEXEC_INSTALL_DIR=$BUILD_DIR/lib \
     -DKDE_INSTALL_USE_QT_SYS_PATHS=ON \
     -DBUILD_TESTING=OFF
     
-    cmake ../$1 \
-    -DCMAKE_TOOLCHAIN_FILE=~/src/mxe/usr/i686-w64-mingw32.static/share/cmake/mxe-conf.cmake \
+    cmake $BUILD_DIR/$1 \
+    -DCMAKE_TOOLCHAIN_FILE=~/src/mxe/usr/i686-w64-mingw32.shared/share/cmake/mxe-conf.cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=../usr \
-    -DLIB_INSTALL_DIR=../lib \
-    -DLIBEXEC_INSTALL_DIR=../lib \
+    -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/usr \
+    -DLIB_INSTALL_DIR=$BUILD_DIR/lib \
+    -DLIBEXEC_INSTALL_DIR=$BUILD_DIR/lib \
     -DKDE_INSTALL_USE_QT_SYS_PATHS=ON \
+    -DKF5Archive_DIR=$BUILD_DIR/lib/cmake/KF5Archive \
+    -DKF5Config_DIR=$BUILD_DIR/lib/cmake/KF5Config \
+    -DKF5CoreAddons_DIR=$BUILD_DIR/lib/cmake/KF5CoreAddons \
+    -DKF5DBusAddons_DIR=$BUILD_DIR/lib/cmake/KF5DBusAddons \
+    -DKF5I18n_DIR=$BUILD_DIR/lib/cmake/KF5I18n \
+    -DKF5Service_DIR=$BUILD_DIR/lib/cmake/KF5Service \
+    -DKF5Crash_DIR=$BUILD_DIR/lib/cmake/KF5Crash \
+    -DKF5WindowSystem_DIR=$BUILD_DIR/lib/cmake/KF5WindowSystem \
     -DBUILD_TESTING=OFF
 
 
 make
-make INSTALL_ROOT=../lib install
-
+make INSTALL_ROOT=$BUILD_DIR/lib install
+sudo make install
 }
 
+buildFramework karchive
+buildFramework kconfig
 buildFramework kcoreaddons
+buildFramework kdbusaddons
+buildFramework ki18n
+buildFramework kwindowsystem
+buildFramework kcrash
+buildFramework kservice
+buildFramework kio
 buildframework kguiaddons
 buildFramework kwidgetsaddons
-buildFramework kdbusaddons
 buildframework kxmlgui
-buildFramework karchive
 buildFramework ktextwidgets
 buildFramework kiconthemes
 buildFramework knotifications
 buildframework knotifyconfig
-buildFramework kio
-buildFramework kcrash
-buildFramework kconfig
 buildframework knewstuff
