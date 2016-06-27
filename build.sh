93068048fdf09e7fa0fb6ffdccb75dc678ba8cc8
Joseph W. Joshua <joejoshw@gmail.com>
2016-06-27 05:28:58 +0300

    Changes to build.sh
    Signed-off-by: Joseph W. Joshua <joejoshw@gmail.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 #MXE_DIR is the location where mxe resides. 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Source path is $SOURCE_DIR" 
-BUILD_DIR=~/src/kdenlinve-win
+BUILD_DIR=~/src/kdenlive-win
 MXE_DIR=$SOURCE_DIR/mxe/mxe
 
 #Step 1: Checkout mxe from github, if not already done
@@ -31,7 +31,7 @@ echo "Compiling MLT and its dependencies using MXE"
 #Copy our MXE settings file to enable shared builds
 cp $SOURCE_DIR/mxe_settings ./settings.mk
 
-make libxml2 libxslt qt5 qtwinextras nsis mlt
+# make libxml2 libxslt qt5 qtwinextras nsis mlt
 
 MXE_INSTALL_PATH=$MXE_DIR/usr/i686-w64-mingw32.shared
 #make docbook-xml
@@ -41,7 +41,7 @@ cd $BUILD_DIR/docbook-xml
 if [ ! -f 'docbook-xml-4.5.zip' ]; then
 	wget http://www.docbook.org/xml/4.5/docbook-xml-4.5.zip
 fi
-unzip docbook-xml-4.5.zip
+unzip -o docbook-xml-4.5.zip
 install -v -d -m755 $MXE_INSTALL_PATH/share/xml/docbook/xml-dtd-4.5  &&
 install -v -d -m755 $MXE_INSTALL_PATH/etc/xml &&
 cp -v -af docbook.cat *.dtd ent/* *.mod \
@@ -60,14 +60,14 @@ fi
 
 tar -xf docbook-xsl-1.79.1.tar.bz2 --strip-components=1
 
-install -v m755 -d $MXE_INSTALL_PATH/share/xml/docbook/xsl-stylesheets-1.79.1
+install -v m755 -d $MXE_INSTALL_PATH/share/xml/docbook/xsl-stylesheets
 
 cp -v -R VERSION assembly common eclipse epub epub3 extensions fo \
 	highlighting html htmlhelp images javahelp lib manpages params \
 	profiling roundtrip slides template tests tools webhelp website \
-	xhtml xhtml-1_1 xhtml5 $MXE_INSTALL_PATH/share/xml/docbook/xsl-stylesheets-1.79.1 
+	xhtml xhtml-1_1 xhtml5 $MXE_INSTALL_PATH/share/xml/docbook/xsl-stylesheets
 
-ln -s VERSION $MXE_INSTALL_PATH/share/xml/docbook/xsl-stylesheets-1.79.1/VERSION.xsl
+ln -sf VERSION $MXE_INSTALL_PATH/share/xml/docbook/xsl-stylesheets/VERSION.xsl
 
 echo "Finished building docbook xsml"
 #end docbook xsl
@@ -81,13 +81,19 @@ function buildFramework {
     echo "Compiling KDE framework ($1)"
     #Download the code
     TAR_FILE_NAME=$1-$FRAMEWORK_VER_MAJOR.$FRAMEWORK_VER_MINOR.tar.xz
-    rm $TAR_FILE_NAME
+    
     if [ ! -f $TAR_FILE_NAME ]; then
         wget http://download.kde.org/stable/frameworks/$FRAMEWORK_VER_MAJOR/$TAR_FILE_NAME
     fi
-    rm $1-$FRAMEWORK_VER_MAJOR.$FRAMEWORK_VER_MINOR -rf
-    tar xvf $TAR_FILE_NAME
-    mkdir -p $BUILD_DIR/frameworks/$1-$FRAMEWORK_VER_MAJOR.$FRAMEWORK_VER_MINOR-build
+    
+    if [ ! -d $1-$FRAMEWORK_VER_MAJOR.$FRAMEWORK_VER_MINOR ]; then
+        tar xvf $TAR_FILE_NAME
+    fi
+    
+    if [ ! -d $BUILD_DIR/frameworks/$1-$FRAMEWORK_VER_MAJOR.$FRAMEWORK_VER_MINOR-build ]; then
+        mkdir -p $BUILD_DIR/frameworks/$1-$FRAMEWORK_VER_MAJOR.$FRAMEWORK_VER_MINOR-build
+    fi
+    
     cd $BUILD_DIR/frameworks/$1-$FRAMEWORK_VER_MAJOR.$FRAMEWORK_VER_MINOR-build
     $MXE_DIR/usr/bin/i686-w64-mingw32.shared-cmake $BUILD_DIR/frameworks/$1-$FRAMEWORK_VER_MAJOR.$FRAMEWORK_VER_MINOR \
         -DCMAKE_BUILD_TYPE=Release \
@@ -97,31 +103,59 @@ function buildFramework {
     make install
 }
 
-buildFramework extra-cmake-modules
-buildFramework sonnet #Success
-buildFramework karchive #Success
-buildFramework kconfig #Success
-buildFramework kcoreaddons #Success
-buildFramework ki18n #Success
-buildFramework kdbusaddons #Success
-buildFramework kwidgetsaddons #Success
-buildFramework kwindowsystem #Success
-buildFramework kcrash #Success
-buildFramework kservice #Success
-buildFramework kguiaddons #Success
-buildFramework kcompletion #Success
-buildFramework kauth #Success
-buildFramework kcodecs #Success
-buildFramework kguiaddons #Success
-buildFramework kconfigwidgets #Success
-buildFramework kitemviews #Success
-buildFramework kiconthemes #Success
-buildFramework ktextwidgets #Success
-buildFramework attica #Success
-buildFramework kglobalaccel #Success
-buildFramework kxmlgui #Success
-buildFramework kdoctools #Failing
-buildFramework kio #Depends on kdoctools
-buildFramework knewstuff #Pending
-buildFramework knotifications #Pending
-buildFramework knotifyconfig #Pending
+# buildFramework extra-cmake-modules
+# buildFramework sonnet 
+# buildFramework karchive 
+# buildFramework kconfig 
+# buildFramework kcoreaddons 
+# buildFramework ki18n 
+# buildFramework kdbusaddons 
+# buildFramework kwidgetsaddons 
+# buildFramework kwindowsystem 
+# buildFramework kcrash 
+# buildFramework kservice 
+# buildFramework kguiaddons 
+# buildFramework kcompletion 
+# buildFramework kauth 
+# buildFramework kcodecs 
+# buildFramework kguiaddons 
+# buildFramework kconfigwidgets 
+# buildFramework kitemviews 
+# buildFramework kiconthemes 
+# buildFramework ktextwidgets 
+# buildFramework attica 
+# buildFramework kglobalaccel 
+# buildFramework kxmlgui 
+# buildFramework kdoctools 
+# buildFramework solid 
+# buildFramework kbookmarks 
+# buildFramework kjobwidgets 
+# buildFramework kio 
+# buildFramework knewstuff 
+# buildFramework kfilemetadata
+# buildFramework kplotting
+# buildFramework knotifyconfig
+buildFramework knotifications
+
+#Build kdenlive
+#git checkout
+cd $BUILD_DIR
+if [ ! -d "kdenlive" ]; then
+    git clone git://anongit.kde.org/kdenlive $BUILD_DIR/kdenlive
+else
+    cd $BUILD_DIR/kdenlive
+    git pull
+fi
+
+cd $BUILD_DIR
+if [ ! -d "kdenlive-build" ]; then
+    mkdir $BUILD_DIR/kdenlive-build
+fi
+
+cd $BUILD_DIR/kdenlive-build
+
+$MXE_DIR/usr/bin/i686-w64-mingw32.shared-cmake $BUILD_DIR/kdenlive \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DKDE_INSTALL_USE_QT_SYS_PATHS=ON \
+        -DBUILD_TESTING=OFF
+    make
